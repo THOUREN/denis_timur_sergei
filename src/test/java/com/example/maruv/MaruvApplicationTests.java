dc4da1b5d1c6a3900717dfadc0075c814d78e5be
THOUREN <informationtehnology012@gmail.com>
2021-09-29 15:39:53 +0600

    Add dts project file

---
+++ src/test/java/com/example/maruv/MaruvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maruv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaruvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
